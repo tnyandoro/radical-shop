f0cd63a15d19c86a9d9ef42a835def457044f0d4
Tendai Nyandoro <tendai1@hotmail.com>
2021-04-01 11:38:31 +0200

    Set classes

--- src/index.js
+++ src/index.js
@@ -7,3 +7,21 @@ const closeCartBtn = document.querySelector('.close-cart');
 const clearCartBtn = document.querySelector('.clear-cart');
 const cartDOM = document.querySelector('.cart');
 const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+
+let cart = []
+
+// Set classes 
+
+// Getting the products list
+class  Products {
+
+}
+
+// Display products
+class UI {
+
+}

684c3b307fd77fe6f3922ac347d8def4bb582ebc
Tendai Nyandoro <tendai1@hotmail.com>
2021-04-01 11:24:14 +0200

    Add the cart DOM

--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ import './style.css';
 const cartBtn = document.querySelector('.cart-btn');
 const closeCartBtn = document.querySelector('.close-cart');
 const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');

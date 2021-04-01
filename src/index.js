6be0fb2ea8520cec496c13724c2d2d060046e769
Tendai Nyandoro <tendai1@hotmail.com>
2021-04-01 11:21:46 +0200

    Create variables for the cart

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+// Variables
+
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');

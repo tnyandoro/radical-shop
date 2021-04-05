70a2d90cde1349c41430936cbdec00fa007a7da2
Tendai Nyandoro <tendai1@hotmail.com>
2021-04-05 14:24:09 +0200

    Add the UI function

--- dist/index.js
+++ dist/index.js
@@ -1,44 +1,49 @@
 // Variables
 
-const cartBtn = document.querySelector('.cart-btn');
-const closeCartBtn = document.querySelector('.close-cart');
-const clearCartBtn = document.querySelector('.clear-cart');
-const cartDOM = document.querySelector('.cart');
-const cartOverlay = document.querySelector('.cart-overlay');
-const cartItems = document.querySelector('.cart-items');
-const cartTotal = document.querySelector('.cart-total');
-const cartContent = document.querySelector('.cart-content');
-const productsDOM = document.querySelector('.products-center');
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
 
-let cart = []
+let cart = [];
 
-// Set classes 
+// Set classes
 
 // Getting the products list
 class Products {
-  async getProducts(){
+  async getProducts() {
     try {
       let result = await fetch("products.json");
-      return result;
+      let data = await result.json();
+      
+      let products = data.items;
+      products = products.map(item => {
+        const {title,price} = item.fields;
+        const {id} = item.sys
+        const image = item.fields.image.fields.file.url;
+        return {title,price,id,image}
+      });
+      return products;
     } catch (error) {
       console.log(error);
     }
   }
 }
 // Display products
-class UI {
-
-}
+class UI {}
 // Local storage
-class Storage {
-
-}
+class Storage {}
 
 // Add listeners
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
   // let data = await result.json();
-  // Get all productsDOM
-  products.getProducts().then(data => console.log(data));
-})
+  // Get all products
+  products.getProducts().then((data) => console.log(data));
+});
